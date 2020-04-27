89b300ad842df08cd47e41f795ef25141ffffa66
Irena Jurasek <irenaskydive@gmail.com>
2020-04-27 11:26:44 +0200

    Create Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Homepage from "./HomePage";
 import AboutPage from "./AboutPage";
+import Header from "./common/Header";
 
 function App() {
-  const route = window.location.pathname;
-  if (route === "/about") return <AboutPage />;
-  return <Homepage />;
+  function getPage() {
+    const route = window.location.pathname;
+    if (route === "/about") return <AboutPage />;
+    return <Homepage />;
+  }
+  return (
+    <div className="container-fluid">
+      <Header />
+      {getPage()}
+    </div>
+  );
 }
 
 export default App;
