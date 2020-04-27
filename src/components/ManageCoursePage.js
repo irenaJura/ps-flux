8b07849f75c5fc5e542f15a29ef42181d70a9277
Irena Jurasek <irenaskydive@gmail.com>
2020-04-27 19:22:25 +0200

    Saving data

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
 
 const ManageCoursePage = (props) => {
   const [course, setCourse] = useState({
@@ -17,10 +18,19 @@ const ManageCoursePage = (props) => {
     });
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    courseApi.saveCourse(course);
+  }
+
   return (
     <>
       <h2>Manage Course</h2>
-      <CourseForm course={course} onChange={handleChange} />
+      <CourseForm
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </>
   );
 };
