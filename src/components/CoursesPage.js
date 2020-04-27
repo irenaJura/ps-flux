60c3ecc2bd180f764702bc58719dc69bad9a4be6
Irena Jurasek <irenaskydive@gmail.com>
2020-04-27 12:06:12 +0200

    Display array in a table via map

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -11,7 +11,31 @@ class CoursesPage extends React.Component {
   }
 
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <>
+        <h2>Courses</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author ID</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.courses.map((course) => {
+              return (
+                <tr key={course.id}>
+                  <td>{course.title}</td>
+                  <td>{course.authorId}</td>
+                  <td>{course.category}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
+    );
   }
 }
 
