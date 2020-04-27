cf2c26602fabff97d9da769568fdf9979c74f50e
Irena Jurasek <irenaskydive@gmail.com>
2020-04-27 11:55:03 +0200

    Create CoursesPage class component

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Homepage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
+import CoursesPage from "./CoursesPage";
 
 function App() {
   function getPage() {
     const route = window.location.pathname;
     if (route === "/about") return <AboutPage />;
+    if (route === "/courses") return <CoursesPage />;
     return <Homepage />;
   }
   return (
