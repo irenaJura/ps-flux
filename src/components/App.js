759a9185006dfa673f847aef3595c69c2145528f
Irena Jurasek <irenaskydive@gmail.com>
2020-04-27 19:29:51 +0200

    Add redirect and toastify notification

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,14 @@ import CoursesPage from "./CoursesPage";
 import { Route, Switch } from "react-router-dom";
 import NotFoundPage from "./NotFoundPage";
 import ManageCoursePage from "./ManageCoursePage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="container-fluid">
       <Header />
+      <ToastContainer autoClose={3000} hideProgressBar />
       <Switch>
         <Route path="/" exact component={Homepage} />
         <Route path="/courses" component={CoursesPage} />
