8e8ea83e98ed44100ae90d4bcec3e04a984b4214
Irena Jurasek <irenaskydive@gmail.com>
2020-04-27 15:32:27 +0200

    Add React Router

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,15 @@ import Homepage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
+import { Route } from 'react-router-dom';
 
 function App() {
-  function getPage() {
-    const route = window.location.pathname;
-    if (route === "/about") return <AboutPage />;
-    if (route === "/courses") return <CoursesPage />;
-    return <Homepage />;
-  }
   return (
     <div className="container-fluid">
       <Header />
-      {getPage()}
+      <Route path="/" exact component={Homepage} />
+      <Route path="/courses" component={CoursesPage} />
+      <Route path="/about" component={AboutPage} />
     </div>
   );
 }
