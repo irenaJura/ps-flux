818d23c5e37f79fc9cc68fbb80b0dcddf6d1f864
Irena Jurasek <irenaskydive@gmail.com>
2020-04-27 11:15:07 +0200

    Create HomePage and AboutPage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import Homepage from "./components/HomePage";
+
+ReactDOM.render(<Homepage />, document.getElementById("root"));
