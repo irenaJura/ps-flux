97642b33b6467d8d10492e1324dd20872fdbcca2
Irena Jurasek <irenaskydive@gmail.com>
2020-04-28 15:45:48 +0200

    Add store change listeners

---
+++ src/stores/courseStore.js
@@ -0,0 +1,17 @@
+import { EventEmitter } from "events";
+
+const CHANGE_EVENT = "change";
+class CourseStore extends EventEmitter {
+  // on, remove, emit methods
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
+  }
+
+  removeChangeListener(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+
+  emitChange() {
+    this.emit(CHANGE_EVENT);
+  }
+}
